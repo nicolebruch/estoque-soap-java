7f8f3dd02dc9edacdcd3dd1ed2bba387316de2f6
nicole <44910604+nicolebruch@users.noreply.github.com>
2026-08-19 21:57:13 -0300

    implementado arquivo q liga o servidor soap

--- src/main/java/br/com/estoque/publisher/EstoquePublisher.java
+++ src/main/java/br/com/estoque/publisher/EstoquePublisher.java
@@ -1,7 +1,14 @@
 package br.com.estoque.publisher;
-//nicole
+
+import br.com.estoque.service.EstoqueServiceImpl;
+
+import javax.xml.ws.Endpoint;
+
 public class EstoquePublisher {
 
     public static void main(String[] args) {
+        Endpoint.publish("http://127.0.0.1:9876/estoque", new EstoqueServiceImpl());
+        System.out.println("web service publicado:");
+        System.out.println("http://127.0.0.1:9876/estoque?wsdl");
     }
 }
